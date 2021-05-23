46c862cbf3debbf396ac2a35728af1195fed23b0
Jaeho Sung <deepsigner@gmail.com>
2021-05-23 15:20:01 +0900

    Update the variables

--- MLStudy_exercise07.py
+++ MLStudy_exercise07.py
@@ -4,6 +4,7 @@ import pandas as pd
 from pandas import Series, DataFrame
 from numpy.random import randint, rand
 import urllib.request
+
 # # 이미지 불러오기
 # urllib.request.urlretrieve(
 #     "https://raw.githubusercontent.com/jaehosung/ml4se_codes/main/exercise07/sample-images_01.txt", "sample-images_01.txt"
@@ -14,16 +15,18 @@ import urllib.request
 
 # 트레이닝 세트 읽기
 df = pd.read_csv('sample-images_01.txt', sep=",", header=None)
-data_num = len(df)
+N = len(df)
 df.head()
 
 # 테스트 용 파라미터
-K = 2
-N = 1
+classifier_num = 3
+iteration_num = 5
 
 
 # 베르누이 분포
 def bern(x, mu):
+    # mu (size = (784,))
+    # x (size = (784,))
     r = 1.0
     for x_i, mu_i in zip(x, mu):
         if x_i == 1:
@@ -36,8 +39,8 @@ def bern(x, mu):
 # 分類結果の表示
 def show_figure(mu, cls):
     fig = plt.figure()
-    for c in range(K):
-        subplot = fig.add_subplot(K, 7, c * 7 + 1)
+    for c in range(classifier_num):
+        subplot = fig.add_subplot(classifier_num, 7, c * 7 + 1)
         subplot.set_xticks([])
         subplot.set_yticks([])
         subplot.set_title('Master')
@@ -45,7 +48,7 @@ def show_figure(mu, cls):
         i = 1
         for j in range(len(cls)):
             if cls[j] == c:
-                subplot = fig.add_subplot(K, 7, c * 7 + i + 1)
+                subplot = fig.add_subplot(classifier_num, 7, c * 7 + i + 1)
                 subplot.set_xticks([])
                 subplot.set_yticks([])
                 subplot.imshow(df.iloc[j].values.reshape(28, 28), cmap=plt.cm.gray_r)
@@ -55,36 +58,38 @@ def show_figure(mu, cls):
     fig.show()
 
 
+# pi: K 이미지 생성기를 선택할 확률 (size=(K,))
+# mu: 총 K개의 이미지 생성기 (size=(K,784))
+
 # 초기 매개 변수의 설정
-mix = [1.0 / K] * K  # mix = pi_list (size = (K, ))
+pi = [1.0 / classifier_num] * classifier_num  # mix = pi_list (size = (K, ))
 
 # TODO 이렇게 수정하면 무슨 문제가 생기는지 알아보기
 # mu = (rand(28*28*K)*0.5+0.25).reshape(K, 28*28) # (K,784) Matrix
-mu = rand(28 * 28 * K).reshape(K, 28 * 28)  # (size = (K, 284))
+mu = rand(28 * 28 * classifier_num).reshape(classifier_num, 28 * 28)  # (size = (K, 284))
 
 # [수정 전]
-for k in range(K):
+for k in range(classifier_num):
     mu[k] /= mu[k].sum()
 
 # [수정 후]
-mu = mu / mu.sum(axis=1)[:, np.newaxis]
+# TODO 이 코드가 왜 필요한지 고민해보기
+# mu = mu / mu.sum(axis=1)[:, np.newaxis]
 
 fig = plt.figure()
 
-for k in range(K):
-    subplot = fig.add_subplot(K, N + 1, k * (N + 1) + 1)
-    subplot.set_xticks([])
-    subplot.set_yticks([])
-    subplot.imshow(mu[k].reshape(28, 28), cmap=plt.cm.gray_r)
+# for k in range(K):
+#     subplot = fig.add_subplot(K, N + 1, k * (N + 1) + 1)
+#     subplot.set_xticks([])
+#     subplot.set_yticks([])
+#     subplot.imshow(mu[k].reshape(28, 28), cmap=plt.cm.gray_r)
 # fig.show()
 
 # N회의 Iteration을 실시
-for iter_num in range(N):
+for iter_num in range(iteration_num):
 
     print("iter_num %d" % iter_num)
-    # E phase
-    resp = DataFrame()
-
+    # expectation (E) step
     # x_n:
     # mu_k :
 
@@ -101,32 +106,39 @@ for iter_num in range(N):
     # mix: pi
     # tmp: r_i_k 길이 K의 행렬
     # resp: r_n_k (데이터 개수, K) 행렬
+    # r_i_k: k번째 분류기로부터 이미지 x_i가 얻어질 가능성
+
+    r_n_k = DataFrame()
 
     # 각각의 이미지 생성기에 소속되는 비율(r_n_k) 계산하기
-    for index, line in df.iterrows():  # line: x_n
-        tmp = []
-        for k in range(K):
-            p = bern(line, mu[k])  # p_u_k : p_u_k(x_n)
-            a = mix[k] * p  # a: pi * p(x_n)
+    for i, x_n in df.iterrows():  # line: x_n (784,)
+        r_i_k = []
+        for k in range(classifier_num):
+            p = bern(x_n, mu[k])  # p_u_k : p_u_k(x_n)
+            a = pi[k] * p  # a: pi * p(x_n) / r_i_k의 분자
 
             if a == 0:
-                tmp.append(0.0)
+                r_i_k.append(0.0)
             else:
                 s = 0.0
-                for kk in range(K):
-                    s += mix[kk] * bern(line, mu[kk])  # r의 분모
-                tmp.append(a / s)
-        resp = resp.append([tmp], ignore_index=True)  # r_n_k_matrix
-
+                for kk in range(classifier_num):
+                    s += pi[kk] * bern(x_n, mu[kk])  # r_i_k의 분모
+                r_i_k.append(a / s)
+        r_n_k = r_n_k.append([r_i_k], ignore_index=True)  # r_n_k (size=(N,K))
+    # maximization (M) step
     # 새로운 이미지 생성기 mu와 이미지 생성기를 선택할 확률 pi 계산하기
-    for k in range(K):
-        nk = resp[k].sum()  # sigma_n(r_n_k)
-        mix[k] = nk / data_num  # pi_k = sigma(n_k) / N
-        for index, line in df.iterrows():
-            mu[k] += line * resp[k][index]  # mu_k 분모: sigma_n(r_n_k * x_n)
-        mu[k] /= nk  # mu_k = sigma_n(r_n_k * x_n) / sigma(r_n_k)
+    # numer: numerator(분자)
+    # denom: denominator(분모)
+    mu_numer = np.zeros(28 * 28 * classifier_num).reshape(classifier_num, 28 * 28)  # (size = (K, 284))
+    for k in range(classifier_num):
+        mu_denom = r_n_k[k].sum()  # sigma_n(r_n_k) dtype: float
+        pi[k] = mu_denom / N  # pi_k = sigma(n_k) / N
+        for i, x_n in df.iterrows():
+            mu_numer[k] += x_n * r_n_k.iloc[i, k]  # mu_k 분모: sigma_n(r_n_k * x_n)
 
-        subplot = fig.add_subplot(K, N + 1, k * (N + 1) + (iter_num + 1) + 1)
+        mu[k] = mu_numer[k] / mu_denom  # mu_k = sigma_n(r_n_k * x_n) / sigma(r_n_k)
+
+        subplot = fig.add_subplot(classifier_num, iteration_num + 1, k * (iteration_num + 1) + (iter_num + 1) + 1)
         subplot.set_xticks([])
         subplot.set_yticks([])
         subplot.imshow(mu[k].reshape(28, 28), cmap=plt.cm.gray_r)
@@ -134,8 +146,8 @@ for iter_num in range(N):
 
 # 트레이닝 세트의 문자를 분류
 cls = []
-for index, line in resp.iterrows():
-    cls.append(np.argmax(line[0:]))
+for i, x_n in r_n_k.iterrows():
+    cls.append(np.argmax(x_n[0:]))
 
 # 분류 결과 표시
 show_figure(mu, cls)
