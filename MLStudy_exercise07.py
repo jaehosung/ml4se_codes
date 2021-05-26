701dd2a85f1b8b12ce008ce045979777dfaf32f0
Jaeho Sung <deepsigner@gmail.com>
2021-05-26 23:45:17 +0900

    Start the 08_02

--- MLStudy_exercise07.py
+++ MLStudy_exercise07.py
@@ -3,37 +3,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import urllib.request
 
-# 이미지 불러오기
+# [트레이닝 셋 불러오기]
+# 0,3,6 데이터 200개씩
 urllib.request.urlretrieve(
-    "https://raw.githubusercontent.com/jaehosung/ml4se_codes/main/exercise07/sample-images_01.txt", "sample-images_01.txt"
+    "https://raw.githubusercontent.com/jaehosung/ml4se_codes/main/exercise07/sample-images_01.txt", "images.txt"
 )
 
-# 트레이닝 셋 할당
-df_training_set = pd.read_csv('sample-images_01.txt', sep=",", header=None) # 트레이닝 셋 (size=(600,784))
-N = len(df_training_set)
-df_training_set.head()
+# 트레이닝 셋 할당, 이미지 1개 28픽셀x28픽셀
+df_training_set = pd.read_csv('images.txt', sep=",", header=None) # 트레이닝 셋 (size=(600,784))
+print(df_training_set.iloc[0].values.reshape(28,28))
 
 # 베르누이 확률 분포
-def bernoulli(x, mu):
-    # mu (size = (784,))
-    # x (size = (784,))
+def bernoulli(x, mu): # x (size = (784,)), mu (size = (784,))
     p = np.prod(mu[x == 1]) * np.prod(1 - mu[x == 0])
     return p
-
-# 파라미터 설정
-K = 3 # 생성기 개수
-iteration_num = 1
-
-# 변수 초기화
-pi = np.full(K, 1 / K) # pi: K번째 이미지 생성기를 선택할 확률 (size=(K,))
-mu = (np.random.rand(28 * 28 * K) * 0.5 + 0.25).reshape(K, 28 * 28) # mu: K개의 이미지 생성기 (size=(K,784))
-r_n_k_final = pd.DataFrame()
-
-# TODO mu의 초기화 방식에 대해서 고민해보기 (추측되는 이유: 0에 가까웠을 때?)
-# mu = np.random.rand(28 * 28 * classifier_num).reshape(classifier_num, 28 * 28)  # (size = (K, 284))
-# mu = mu / mu.sum(axis=1)[:, np.newaxis]
-
-fig_generators, axs_generators = plt.subplots(K, iteration_num+1)  # 생성기의 변화 시각화
 
 # 생성기 시각화
 def draw_generator(axs_generators, iter_num, mu):
@@ -42,56 +25,12 @@ def draw_generator(axs_generators, iter_num, mu):
         axs_generators[k, iter_num].set_yticks([])
         axs_generators[k, iter_num].imshow(mu[k].reshape(28, 28), cmap=plt.cm.gray_r)
 
+# 파라미터 설정
+N = len(df_training_set) # 데이터 개수 (600)
+K = 3 # 생성기 개수
+iteration_num = 10# 생성기 생성 반복 횟수
 
-# 이미지 생성기 초기값 시각화
-draw_generator(axs_generators, 0, mu)
-
-
-for iter_num in range(iteration_num):
-    print("iter_num %d" % iter_num)
-
-    # expectation (E) step
-    # 각각의 이미지 생성기에 소속되는 비율(r_n_k) 계산하기
-    r_n_k = pd.DataFrame()
-
-    for _, x_n in df_training_set.iterrows():  # line: x_n (784,)
-        r_i_k = []
-        for k in range(K):
-            p = bernoulli(x_n, mu[k])  # p_u_k : p_u_k(x_n)
-            r_i_k_numer = pi[k] * p  # a: pi * p(x_n) / r_i_k의 분자
-
-            if r_i_k_numer == 0:
-                r_i_k.append(0.0)
-            else:
-                r_i_k_denom = 0.0  # sigma(pi * P_mu_k)
-                for kk in range(K):
-                    r_i_k_denom += pi[kk] * bernoulli(x_n, mu[kk])  # r_i_k의 분모
-                r_i_k.append(r_i_k_numer / r_i_k_denom)
-
-        r_n_k = r_n_k.append([r_i_k], ignore_index=True)  # r_n_k (size=(N,K))
-
-    # maximization (M) step
-    # 새로운 이미지 생성기 mu와 이미지 생성기를 선택할 확률 pi 계산하기
-    mu_numer = np.zeros(28 * 28 * K).reshape(K, 28 * 28)  # (size = (K, 284))
-
-    for k in range(K):
-        mu_denom = r_n_k[k].sum()  # sigma_n(r_n_k) dtype: float
-        pi[k] = mu_denom / N  # pi_k = sigma(n_k) / N
-        for i, x_n in df_training_set.iterrows():
-            mu_numer[k] += x_n * r_n_k.iloc[i, k]  # mu_k 분모: sigma_n(r_n_k * x_n)
-        mu[k] = mu_numer[k] / mu_denom  # mu_k = sigma_n(r_n_k * x_n) / sigma(r_n_k)
-
-        # 생성기 시각화
-    draw_generator(axs_generators, iter_num + 1, mu)
-
-    r_n_k_final = r_n_k.copy()
-
-r_argmax = list(r_n_k_final.idxmin(axis = 1)) # 가장 높은 확률로 소속될 생성기의 인덱스
-
-
-fig_result, axs_result = plt.subplots(K, 7)
-
-# 분류 결과의 표시
+# 분류 결과의 시각화 함수
 def draw_result(mu, r_argmax, axs):
     for c in range(K):
         # 이미지 생성기 시각화
@@ -110,7 +49,81 @@ def draw_result(mu, r_argmax, axs):
                 i += 1
                 if i > 6:
                     break
-# 분류 결과 표시
-draw_result(mu, r_argmax,axs_result)
 
+
+# E(expectation) step
+# 각 이미지가 k 이미지 생성기를 통해서 얻어질 가능성 계산
+def calc_expectation(df_training_set, pi, mu):
+    r_n_k = np.empty(shape=(0, K))  # r_n_k: 데이터가 k번째 이미지 생성기로부터 이미지가 얻어질 가능성
+
+    # r_n_k 계산하기
+    for _, x_n in df_training_set.iterrows():  # N개의 데이터에 대해서 차례대로 계산, x_n (size=(784,))
+
+        r_i_k = np.array([])
+
+        for k in range(K):  # K개의 이미지 생성기에 대해서 차례대로 계산
+            p_mu_k = bernoulli(x_n, mu[k])  # p_u_k : p_u_k(x_n)
+
+            # r_i_k_numer 계산
+            r_i_k_numer = pi[k] * p_mu_k  # r_i_k_numer(numerator): pi * p_mu_k(x_n)
+
+            # r_i_k_denom 계산
+            r_i_k_denom = 0.0  # r_i_k_denom(denominator): sigma(pi * P_mu_k)
+            for kk in range(K):
+                r_i_k_denom += pi[kk] * bernoulli(x_n, mu[kk])
+
+                # r_i_k = r_i_k_numer / r_i_k_denom
+            r_i_k = np.hstack((r_i_k, r_i_k_numer / r_i_k_denom))
+        r_n_k = np.vstack((r_n_k, r_i_k))  # r_n_k (size=(N,K))
+
+    return r_n_k
+
+
+# M(maximization) step
+# 새로운 이미지 생성기 mu와 이미지 생성기를 선택할 확률 pi 계산
+def calc_new_generators(df_training_set, r_n_k, pi, K):
+    mu_numer = np.zeros(28 * 28 * K).reshape(K, 28 * 28)  # mu_numer(numerator) (size = (K, 284))
+    for k in range(K):  # K개의 이미지 생성기에 대해서 차례대로 계산
+        mu_denom = r_n_k[:, k].sum()  # mu_denom = sigma_n(r_n_k) (dtype: float)
+        pi[k] = mu_denom / N  # pi_k = sigma(n_k) / N
+        for i, x_n in df_training_set.iterrows():  # N개의 데이터에 대해서 차례대로 계산
+            mu_numer[k] += x_n * r_n_k[i][k]  # mu_k 분모: sigma_n(r_n_k * x_n)
+        mu[k] = mu_numer[k] / mu_denom  # mu_k = sigma_n(r_n_k * x_n) / sigma(r_n_k)
+
+    return mu, pi
+
+
+# EM 알고리즘을 사용하여 이미지 생성기 만들기
+
+# 변수 초기화
+pi = np.full(K, 1 / K)  # pi: K번째 이미지 생성기를 선택할 확률 (size=(K,))
+# mu: K개의 이미지 생성기 (size=(K,784))
+mu = (np.random.rand(28 * 28 * K) * 0.5 + 0.25).reshape(K, 28 * 28)
+# r_n_k: n번째 이미지가 k번째 이미지 생성기로부터 이미지가 얻어질 가능성, r_n_k_final: 계산 반복을 끝냈을 때 r_n_k 값
+r_n_k_final = np.array([])
+
+# 생성기의 변화 시각화
+fig_generators, axs_generators = plt.subplots(K, iteration_num + 1)  # axs_generators (size=(K, iteration_num+1))
+
+# 이미지 생성기 초기값 시각화
+draw_generator(axs_generators, 0, mu)
+
+for iter_num in range(iteration_num):
+    print("iter_num %d" % iter_num)
+
+    # 각 이미지가 k 이미지 생성기를 통해서 얻어질 가능성 계산
+    r_n_k = calc_expectation(df_training_set, pi, mu)
+    # 새로운 이미지 생성기 mu와 이미지 생성기를 선택할 확률 pi 계산
+    mu, pi = calc_new_generators(df_training_set, r_n_k, pi, K)
+
+    # 이미지 생성기 시각화
+    draw_generator(axs_generators, iter_num + 1, mu)
+
+    # 계산 반복을 끝냈을 때 r_n_k 값 저장
+    r_n_k_final = r_n_k.copy()
+
+# 분류 결과 표시
+fig_result, axs_result = plt.subplots(K, 7)
+r_argmax = list(r_n_k_final.argmax(axis=1))  # 가장 높은 확률로 소속될 생성기의 인덱스
+draw_result(mu, r_argmax, axs_result)
 fig_result.show()
--- MLStudy_exercise08_02.py
+++ MLStudy_exercise08_02.py
@@ -2,77 +2,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from pandas import Series, DataFrame
-from numpy.random import normal, multivariate_normal
 
 beta = 1.0 / (0.3) ** 2  # 진정한 분포의 분산
 alpha = 1.0 / 100 ** 2  # 사전분포 분산
-order = 9  # 다항식 차수
-
-## 이해 완료 ##
+M = 9  # 다항식 차수
+N_list = [4, 5, 10, 100]
 
 # 트레이닝 셋 {x_n,y_n} (n=1...N) 을 준비
 def create_dataset(num):
-    dataset = DataFrame(columns=["x", "y"])
+    dataset = pd.DataFrame(columns=["x", "y"])
     for i in range(num):
         x = float(i) / float(num - 1)
-        y = np.sin(2.0 * np.pi * x) + normal(scale=0.3)
-        dataset = dataset.append(Series([x, y], index=["x", "y"]), ignore_index=True)
+        y = np.sin(2.0 * np.pi * x) + np.random.normal(scale=0.3)
+        dataset = dataset.append(pd.Series([x, y], index=["x", "y"]), ignore_index=True)
     return dataset
+
 
 
 # 사후분포에 기반한 추정곡선 및 사후분포의 평균과 분산을 계산
 def resolve(dataset, m):
+
+    # phis 생성
     t = dataset.y
-    phis = DataFrame()
+    phis = pd.DataFrame() # (size=(N,M+1))
+
     for i in range(0, m + 1):
         p = dataset.x ** i
         p.name = "x**%d" % i
         phis = pd.concat([phis, p], axis=1)
+
+
+    # 분산(S) 계산
+    phiphi = 0
     for index, line in phis.iterrows():
-        phi = DataFrame(line)
-        if index == 0:
-            phiphi = np.dot(phi, phi.T)
-        else:
-            phiphi += np.dot(phi, phi.T)
-    s_inv = alpha * DataFrame(np.identity(m + 1)) + beta * phiphi
+        phi = pd.DataFrame(line)
+        phiphi += np.dot(phi, phi.T)
+        #phiphi: (size=(M+1,M+1))
+    s_inv = alpha * pd.DataFrame(np.identity(m + 1)) + beta * phiphi
     s = np.linalg.inv(s_inv)  # 사후분포의 공분산행렬
 
-    # 평균 m (x)
+    # 평균 m(x)
     def mean_fun(x0):
-        phi_x0 = DataFrame([x0 ** i for i in range(0, m + 1)])
+        phi_x0 = pd.DataFrame([x0 ** i for i in range(0, m + 1)])
+        tmp = 0
         for index, line in phis.iterrows():
-            if index == 0:
-                tmp = t[index] * line
-            else:
-                tmp += t[index] * line
-        return (beta * np.dot(np.dot(phi_x0.T, s), DataFrame(tmp))).flatten()
+            tmp += t[index] * line
+        return (beta * np.dot(np.dot(phi_x0.T, s), pd.DataFrame(tmp))).flatten()
 
     # 표준편차 s(x)
     def deviation_fun(x0):
-        phi_x0 = DataFrame([x0 ** i for i in range(0, m + 1)])
+        phi_x0 = pd.DataFrame([x0 ** i for i in range(0, m + 1)])
         deviation = np.sqrt(1.0 / beta + np.dot(np.dot(phi_x0.T, s), phi_x0))
         return deviation.diagonal()
 
+    tmp = 0
     for index, line in phis.iterrows():
-        if index == 0:
-            tmp = t[index] * line
-        else:
-            tmp += t[index] * line
-    mean = beta * np.dot(s, DataFrame(tmp)).flatten()  # 事後分布の平均
+        tmp += t[index] * line
+    mean = beta * np.dot(s, pd.DataFrame(tmp)).flatten()  # 사후분포 평균
 
     return mean_fun, deviation_fun, mean, s
 
+####################################################### 이해 완료 ########################################################
 
 # Main
-df_ws = DataFrame()
+df_ws = pd.DataFrame()
 
 fig1 = plt.figure()
 fig2 = plt.figure()
-for c, num in enumerate([4, 5, 10, 100]):  # 트레이닝 셋의 데이터 수
+for c, num in enumerate(N_list):  # 트레이닝 셋의 데이터 수
     train_set = create_dataset(num)
-    mean_fun, deviation_fun, mean, sigma = resolve(train_set, order)
-    ws_samples = DataFrame(multivariate_normal(mean, sigma, 4))
+    mean_fun, deviation_fun, mean, sigma = resolve(train_set, M)
+    ws_samples = pd.DataFrame(np.random.multivariate_normal(mean, sigma, 4))
 
     subplot1 = fig1.add_subplot(2, 2, c + 1)
     subplot1.set_xlim(-0.05, 1.05)
@@ -118,5 +118,6 @@ for c, num in enumerate([4, 5, 10, 100]):  # 트레이닝 셋의 데이터 수
     for index, ws in ws_samples.iterrows():
         liney = f(linex, ws)
         subplot2.plot(linex, liney, color="red", linestyle="--")
+
 fig1.show()
 fig2.show()
